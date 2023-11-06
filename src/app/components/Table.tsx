8b13af5f5c5d3dac218798dffa462d76609c9520
Jaco Baldrich <jb@jacobaldrich.com>
2023-11-06 11:57:40 +0100

    add solution when not correct

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -14,10 +14,10 @@ async function validate(answers: Answers): Promise<Answers> {
     pair[1].translation.verb,
   ))).map(validated =>
     [validated.id, {
-      translation: { verb: validated.translation.attempt, isValid: validated.translation.isCorrect() },
-      infinitive: { verb: validated.infinitive.attempt, isValid: validated.infinitive.isCorrect() },
-      pastTense: { verb: validated.pastTense.attempt, isValid: validated.pastTense.isCorrect() },
-      pastParticiple: { verb: validated.pastParticiple.attempt, isValid: validated.pastParticiple.isCorrect() },
+      translation: { verb: validated.translation.attempt, answer: validated.translation.answer, isValid: validated.translation.isCorrect() },
+      infinitive: { verb: validated.infinitive.attempt, answer: validated.infinitive.answer, isValid: validated.infinitive.isCorrect() },
+      pastTense: { verb: validated.pastTense.attempt, answer: validated.pastTense.answer, isValid: validated.pastTense.isCorrect() },
+      pastParticiple: { verb: validated.pastParticiple.attempt, answer: validated.pastParticiple.answer, isValid: validated.pastParticiple.isCorrect() },
     }]
   ));
   return new Promise<Answers>((resolve, reject) => {
@@ -45,6 +45,7 @@ type Verb2 = {
 
 type Answer = {
   verb: string;
+  answer: string | null;
   isValid: null | boolean;
 }
 
@@ -53,10 +54,10 @@ type Answers = Map<string, Verb2>;
 export const Table = ({ verbs }: TableProps) => {
   const [answers, setAnswers] = useState<Answers>(new Map(verbs.map(verb =>
     [verb.id, {
-      translation: { verb: verb.translation ?? '', isValid: null },
-      infinitive: { verb: verb.infinitive ?? '', isValid: null },
-      pastTense: { verb: verb.pastTense ?? '', isValid: null },
-      pastParticiple: { verb: verb.pastParticiple ?? '', isValid: null }
+      translation: { verb: verb.translation ?? '', isValid: null, answer: null },
+      infinitive: { verb: verb.infinitive ?? '', isValid: null, answer: null },
+      pastTense: { verb: verb.pastTense ?? '', isValid: null, answer: null },
+      pastParticiple: { verb: verb.pastParticiple ?? '', isValid: null, answer: null }
     }]
   )));
   const [error, setError] = useState<Error | null>(null);
@@ -95,10 +96,10 @@ export const Table = ({ verbs }: TableProps) => {
           {
             [...answers].map(answer =>
               <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700" key={answer[0]}>
-                <Cell verbId={answer[0]} name='translation' value={answer[1].translation.verb} isValid={answer[1].translation.isValid} onChange={handleInputChange} />
-                <Cell verbId={answer[0]} name='infinitive' value={answer[1].infinitive.verb} isValid={answer[1].infinitive.isValid} onChange={handleInputChange} />
-                <Cell verbId={answer[0]} name='pastTense' value={answer[1].pastTense.verb} isValid={answer[1].pastTense.isValid} onChange={handleInputChange} />
-                <Cell verbId={answer[0]} name='pastParticiple' value={answer[1].pastParticiple.verb} isValid={answer[1].pastParticiple.isValid} onChange={handleInputChange} />
+                <Cell verbId={answer[0]} name='translation' value={answer[1].translation.verb} answer={answer[1].translation.answer} isValid={answer[1].translation.isValid} onChange={handleInputChange} />
+                <Cell verbId={answer[0]} name='infinitive' value={answer[1].infinitive.verb} answer={answer[1].infinitive.answer} isValid={answer[1].infinitive.isValid} onChange={handleInputChange} />
+                <Cell verbId={answer[0]} name='pastTense' value={answer[1].pastTense.verb} answer={answer[1].pastTense.answer} isValid={answer[1].pastTense.isValid} onChange={handleInputChange} />
+                <Cell verbId={answer[0]} name='pastParticiple' value={answer[1].pastParticiple.verb} answer={answer[1].pastParticiple.answer} isValid={answer[1].pastParticiple.isValid} onChange={handleInputChange} />
               </tr>
             )
           }
@@ -132,13 +133,14 @@ type CellProps = {
   verbId: string
   name: string
   value: string
+  answer: string | null
   isValid: boolean | null
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Cell = ({ verbId, name, value, isValid, onChange }: CellProps) => {
+export const Cell = ({ verbId, name, value, answer, isValid, onChange }: CellProps) => {
   if (isValid !== null && isValid !== undefined && !isValid) {
-    return (<td className="px-6 py-4 bg-red-700">{value}</td>)
+    return (<td className="px-6 py-4 bg-red-700"><s>{value}</s> {'->'} <strong>{answer}</strong></td>)
   }
   return (
     <td className="px-6 py-4">
